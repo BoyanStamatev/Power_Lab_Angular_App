e6f9624c1f03ca93cd50f54bca574bd3543f44a4
BoyanStamatev <boyanstamatev@gmail.com>
2020-01-28 22:06:07 +0200

    Application structure with navigation and footer added

---
+++ PowerLab-App/src/app/components/index.ts
@@ -0,0 +1,7 @@
+import { NavigationComponent } from './shared/navigation/navigation.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
+export const sharedComponents = [
+    NavigationComponent,
+    FooterComponent
+]
